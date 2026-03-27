dc1edcd3be6024bdc0c72a289bc100f3ecf83760
Varis <movaris85@gmail.com>
2026-03-27 23:31:32 +0530

    added functionality to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,36 +1,59 @@
 import React from "react";
 import { useAuth } from "../features/auth/hooks/useAuth";
+import { Link, useLocation } from "react-router";
+
 const Navbar = () => {
   const { handleLogout } = useAuth();
+  const location = useLocation();
+
+  const navItem =
+    "relative text-gray-300 transition duration-300 ease-in-out transform hover:scale-110 hover:text-white";
+
   return (
     <nav className="w-full flex justify-center pt-6 absolute top-0 left-0 z-50">
       <div
-        className="flex items-center gap-8 px-8 py-3 
+        className="flex items-center gap-10 px-10 py-3 
         border border-white/10 
         rounded-full 
-        bg-white/5 backdrop-blur-md"
+        bg-white/5 backdrop-blur-md shadow-lg"
       >
         {/* Logo */}
-        <span className="text-white font-extrabold ">AI Interview</span>
+        <Link
+          to="/"
+          className="text-white font-extrabold text-lg tracking-wide hover:scale-105 transition duration-300"
+        >
+          AI Interview
+        </Link>
 
         {/* Menu */}
-        <span className="text-gray-300 hover:text-white cursor-pointer">
-          Home
-        </span>
+        <Link
+          to="/home"
+          className={`${navItem} ${
+            location.pathname === "/home" ? "text-white" : ""
+          }`}
+        >
+          Plan Interview
+        </Link>
 
-        <span className="text-gray-300 hover:text-white cursor-pointer">
-          Interview
-        </span>
+        <Link
+          to="/interview/123"
+          className={`${navItem} ${
+            location.pathname.includes("/interview") ? "text-white" : ""
+          }`}
+        >
+          Report
+        </Link>
 
         {/* Logout Button */}
         <button
           onClick={handleLogout}
-          className="flex items-center justify-center text-sm px-4 py-2
-                     rounded-full
-                   bg-pink-500/90 hover:bg-pink-500
-                   text-white transition cursor-pointer"
+          className="ml-4 px-5 py-2 rounded-full text-sm font-medium
+                     bg-gradient-to-r from-pink-500 to-rose-500
+                     hover:from-pink-600 hover:to-rose-600
+                     text-white shadow-md
+                     transform hover:scale-105 transition duration-300 cursor-pointer"
         >
-          Logout
+          Sign Out
         </button>
       </div>
     </nav>
